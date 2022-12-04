133c6bb277aa9ef8d75a86e7d307ce2eebbdc5ea
Arew <noobterr@yandex.ru>
2022-12-04 20:46:47 +0300

    AtomDestiny::Core added

---
+++ Source/AtomDestiny/Core/AtomDestinyCore.cpp
@@ -0,0 +1,18 @@
+﻿#include "AtomDestinyCore.h"
+
+struct AtomDestiny::Core::CoreData
+{
+};
+
+AtomDestiny::Core::Core():
+    m_impl(std::make_unique<CoreData>())
+{
+}
+
+AtomDestiny::Core::~Core() = default;
+
+AtomDestiny::Core& AtomDestiny::Core::Instance()
+{
+    static Core core;
+    return core;
+}
--- Source/AtomDestiny/Core/AtomDestinyCore.h
+++ Source/AtomDestiny/Core/AtomDestinyCore.h
@@ -1,5 +1,7 @@
 ﻿#pragma once
 
+#include <memory>
+
 #include <Core/Public/Templates/SharedPointer.h>
 
 #include <Engine/Classes/GameFramework/Actor.h>
@@ -9,4 +11,25 @@ namespace AtomDestiny
     using GameObject = TSharedPtr<AActor>;
     using GameObjectWeak = TWeakPtr<AActor>;
 
+    ///
+    /// Represents super base core class for AtomDestiny game.
+    /// It has Singleton realization, be aware to store some data.
+    /// To has a better compile time and scaling, stores data as pointer implementation.
+    ///
+    class Core : public FNoncopyable
+    {
+    protected:
+        Core();
+        ~Core();
+
+    public:
+
+        // Returns Core object as Meyers singleton
+        static Core& Instance();
+
+    private:
+        struct CoreData;
+        std::unique_ptr<CoreData> m_impl;
+    };
+    
 } // namespace AtomDestiny
