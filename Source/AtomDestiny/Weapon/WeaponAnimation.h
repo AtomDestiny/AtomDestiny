ca842cd73be7eb32199583591a2f77d64ad767cf
Arew <noobterr@yandex.ru>
2022-12-06 19:58:20 +0300

    Base unit behaviour - add IWeapon animation interface

---
+++ Source/AtomDestiny/Weapon/WeaponAnimation.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+namespace AtomDestiny::Weapon
+{
+    ///
+    /// Base interface for weapon animation
+    ///
+    struct IWeaponAnimation
+    {
+        virtual ~IWeaponAnimation() = default;
+
+        // Returns weapon ready status
+        virtual bool IsReady() = 0;
+
+        // Plays animation
+        virtual void Animate() = 0;
+
+        // Sets default state of animation
+        virtual void SetDefaultState() = 0;
+    };
+
+} // namespace AtomDestiny
