b336e98e8866bb91de22ec797a2675f8289caa1b
Arew <noobterr@yandex.ru>
2022-12-07 21:52:31 +0300

    Base unit behaviour - try new macro

--- Source/AtomDestiny/Core/Macros.h
+++ Source/AtomDestiny/Core/Macros.h
@@ -8,4 +8,13 @@
     #define __FUNC_NAME__ __PRETTY_FUNCTION__
 #endif
 
-#define PURE_VIRTUAL_METHOD PURE_VIRTUAL(__FUNC_NAME__)
+constexpr bool IsVoid(const std::string_view function)
+{
+    return function.find_first_of("void") != std::string_view::npos;
+}
+
+#define PURE_VIRTUAL_METHOD { \
+    constexpr auto res = IsVoid(__FUNC_NAME__); \
+    PURE_VIRTUAL(__FUNC_NAME__) \
+    if constexpr (!res) return {}; \
+}
