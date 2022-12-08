2976794a3b93d6dc1f583a6e9e1a4f6b8e9b2161
Arew <noobterr@yandex.ru>
2022-12-08 22:32:00 +0300

    Base unit behaviour - add partial WeaponBase realization // TODO: implement full entity

--- Source/AtomDestiny/Weapon/WeaponBase.cpp
+++ Source/AtomDestiny/Weapon/WeaponBase.cpp
@@ -1,1 +1,190 @@
 ﻿#include "WeaponBase.h"
+
+void UWeaponBase::InitializeComponent()
+{
+    Super::InitializeComponent();
+    
+    m_weaponTransform = this;
+        
+    m_currentDamage = m_damage;
+    m_currentAttackRange = m_attackRange;
+    m_currentExplosionRadius = m_explosionRadius;
+    m_currentCriticalChance = m_criticalChance;
+    m_currentCriticalRate = m_criticalRate;
+    m_currentReloadTime = m_reloadTime;
+
+    m_minShotSqrtDistance = m_minShotDistance * m_minShotDistance;
+
+    if (m_projectileBlueprint.IsValid())
+    {
+        const TStrongObjectPtr ptr { m_projectileBlueprint };
+        AtomDestiny::ObjectPool::Instance().Preload(ptr, BlueprintPreloadCount);
+    }
+
+    const std::vector params = {
+        EObjectParameters::Reload,
+        EObjectParameters::ExplosionRadius,
+        EObjectParameters::Damage,
+        EObjectParameters::CriticalRate,
+        EObjectParameters::CriticalChance,
+        EObjectParameters::Range
+    };
+        
+    AddNewParameters(params);
+}
+
+void UWeaponBase::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    m_weaponAnimation = TObjectPtr<IWeaponAnimation> { GetOwner()->FindComponentByClass<IWeaponAnimation>() };
+
+    if (m_useRaycast && !m_useFriendlyFire)
+        ExcludeSameLayer();
+}
+
+void UWeaponBase::EndPlay(const EEndPlayReason::Type type)
+{
+    Super::EndPlay(type);
+    m_firing = false;
+    // StopAllCoroutines();
+}
+
+void UWeaponBase::Activate(bool state)
+{
+    Super::Activate(state);
+    m_firing = false;
+    // StopAllCoroutines();
+}
+
+// ReSharper disable once CppMemberFunctionMayBeStatic
+void UWeaponBase::ExcludeSameLayer()
+{
+    // raycastMask = raycastMask.RemoveFromMask(gameObject.layer);
+}
+
+bool UWeaponBase::IsFiring() const
+{
+    return m_firing;
+}
+
+bool UWeaponBase::IsAoe() const
+{
+    return m_explosionRadius > 0;
+}
+
+double UWeaponBase::GetDamage() const
+{
+    return m_currentDamage;
+}
+
+bool UWeaponBase::IsCritical() const
+{
+    return (m_criticalChance > 0 && m_criticalRate > 1);
+}
+
+bool UWeaponBase::IsShouldStop() const
+{
+    return m_stopWhenAttack;
+}
+
+bool UWeaponBase::IsRaycasted() const
+{
+    return m_useRaycast;
+}
+
+bool UWeaponBase::IsFriendlyFireAvailable() const
+{
+    return m_useFriendlyFire;
+}
+
+double UWeaponBase::GetReloadTime() const
+{
+    return m_currentReloadTime;
+}
+
+double UWeaponBase::GetBaseReloadTime() const
+{
+    return m_reloadTime;
+}
+
+void UWeaponBase::SetTarget(AActor* target)
+{
+    m_target = TWeakObjectPtr<AActor> { target };
+}
+
+double UWeaponBase::GetAttackRange() const
+{
+    return m_currentAttackRange;
+}
+
+double UWeaponBase::GetMinAttackRange() const
+{
+    return m_minShotDistance;
+}
+
+bool UWeaponBase::IsRotated() const
+{
+    return m_rotatedWeapon;
+}
+
+EWeaponType UWeaponBase::GetWeaponType() const
+{
+    return m_weaponType;
+}
+
+double UWeaponBase::GetBaseDamage() const
+{
+    return m_damage;
+}
+
+double UWeaponBase::GetBaseAttackRange() const
+{
+    return m_attackRange;
+}
+
+double UWeaponBase::GetCriticalChance() const
+{
+    return m_currentCriticalChance;
+}
+
+double UWeaponBase::GetBaseCriticalChance() const
+{
+    return m_criticalChance;
+}
+
+double UWeaponBase::GetCriticalRate() const
+{
+    return m_currentCriticalRate;
+}
+
+double UWeaponBase::GetBaseCriticalRate() const
+{
+    return m_criticalRate;
+}
+
+void UWeaponBase::RotateToTarget()
+{
+    if (!m_target.IsValid())
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Target for weapon is invalid"));
+        return;
+    }
+
+    // calculate vector on target
+    const auto& weaponLocation = m_weaponTransform->GetComponentTransform().GetLocation();
+    FVector targetVector = m_target->GetTransform().GetLocation() - weaponLocation;
+    targetVector.Y = 0;
+    
+    // angle between unit and target
+    const auto angle = Vector3.Angle(target, weaponLocation.ForwardVector);
+
+    // check angle
+    isRotatedOnTarget = (Mathf.Abs(angle) < attackAngle);
+
+    var lookVector = Vector3.RotateTowards(weaponTransform.forward, target, rotateSpeed * Time.deltaTime, 0.1f);
+
+    // rotate
+    weaponTransform.rotation = Quaternion.LookRotation(lookVector);
+
+}
--- Source/AtomDestiny/Weapon/WeaponBase.h
+++ Source/AtomDestiny/Weapon/WeaponBase.h
@@ -1,18 +1,449 @@
 ﻿#pragma once
 
-#include "AtomDestiny/Weapon/Weapon.h"
-#include "AtomDestiny/Core/ADObject/ADObject.h"
+#include <AtomDestiny/Weapon/Weapon.h>
+#include <AtomDestiny/Weapon/WeaponAnimation.h>
 
-// #include "WeaponBase.generated.h"
+#include <AtomDestiny/Core/ADObject/ADObject.h>
+#include <AtomDestiny/Core/ObjectPool/ActorPool.h>
+
+#include "WeaponBase.generated.h"
 
 ///
 /// Represents base abstract weapon, any weapon realizations should use it as base class
 ///
-// UCLASS(Blueprintable)
-// class ATOMDESTINY_API UWeaponBase : public UObject
-// {
-//     GENERATED_BODY()
-//
-// public:
-//     UWeaponBase() = default;
-// };
+UCLASS(Abstract, Blueprintable)
+class ATOMDESTINY_API UWeaponBase : public UADObject, public IWeapon
+{
+    GENERATED_BODY()
+
+protected:
+    
+    // Time to reload weapon
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Reload time"))
+    double m_reloadTime = 2.0;
+    
+    // Max shot azimuth angle
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Attack angle"))
+    double m_attackAngle = 2.0;
+    
+    // Weapon rotation speed
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Rotation speed"))
+    double m_rotateSpeed = 1.5;
+    
+    // Base weapon damage
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Damage"))
+    double m_damage = 2.0;
+    
+    // Base weapon critical rate, multiply on base damage
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Critical rate"))
+    double m_criticalRate = 2.0;
+    
+    // Base weapon critical chance in percent
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Critical chance"))
+    double m_criticalChance = 15.0;
+    
+    // Base weapon attack range
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Attack range"))
+    double m_attackRange = 5.0;
+    
+    // Base projectile explosion radius if weapon is aoe
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Explosion radius"))
+    double m_explosionRadius = 1.0;
+    
+    // Weapon type
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Weapon type"))
+    EWeaponType m_weaponType;
+    
+    // Weapon rotation ability
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Rotated weapon"))
+    bool m_rotatedWeapon = false;
+    
+    // Weapon gives command to stop if it is firing
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Stop when attack"))
+    bool m_stopWhenAttack = true;
+    
+    // Weapon animator reference
+    TScriptInterface<IWeaponAnimation> m_weaponAnimation;
+    
+    // Use weapon projectiles as a child
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Use projectile as child"))
+    bool m_useProjectileAsChild = false;
+    
+    // Use raycast when check fire ability
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Use raycast"))
+    bool m_useRaycast = true;
+    
+    /// Fire ability thought friendly units
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Use friendly fire"))
+    bool m_useFriendlyFire = true;
+    
+    // Projectile blueprint with projectile interface that spawns by weapon
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Projectile blueprint"))
+    TWeakObjectPtr<AActor> m_projectileBlueprint;
+
+    // Minimal distance to shot
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Minimal distance to shot"))
+    double m_minShotDistance = 0;
+    
+    // Main weapon target, driven by ILogic
+    TWeakObjectPtr<AActor> m_target = nullptr;
+    
+    /// Firing state
+    bool m_firing = false;
+    
+    // Rotated on target state
+    bool m_isRotatedOnTarget = false;
+    
+    // Hashed weapon transform
+    TWeakObjectPtr<USceneComponent> m_weaponTransform = nullptr;
+    
+    // Preload spawn blueprints count at pool
+    inline static int BlueprintPreloadCount = 5;
+    
+    /// Current real damage
+    double m_currentDamage = 0;
+    
+    /// Current real attack range
+    double m_currentAttackRange = 0;
+    
+    // Current weapon explosion radius
+    double m_currentExplosionRadius = 0;
+    
+    // Current weapon critical chance
+    double m_currentCriticalChance = 0;
+    
+    // Current weapon critical rate
+    double m_currentCriticalRate = 0;
+    
+    // Current weapon reload time
+    double m_currentReloadTime = 0;
+    
+    // Current sqrt min shot distance
+    double m_minShotSqrtDistance = 0;
+    
+private:
+    
+    // Setups weapon
+    virtual void InitializeComponent() override;
+    
+    // Before first frame
+    virtual void BeginPlay() override;
+    
+    // Destroy message
+    virtual void EndPlay(const EEndPlayReason::Type type) override;
+
+    // Active to false
+    virtual void Activate(bool state) override;
+
+    // Removes actors layer from raycast mask
+    void ExcludeSameLayer();
+
+public:
+    
+    // Returns fire rate, should be realized at concrete weapon
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual double GetFireRate() const override PURE_VIRTUAL_RETURN_METHOD
+
+    // Main shot for weapon, called by Logic
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual void Fire() override PURE_VIRTUAL_METHOD
+    
+    // Returns weapon firing state
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual bool IsFiring() const override;
+    
+    // Returns aoe flag
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual bool IsAoe() const override;
+    
+    // Returns current damage
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual double GetDamage() const override;
+    
+    // Returns critical flag
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual bool IsCritical() const override;
+    
+    // Returns stop when attack flag
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual bool IsShouldStop() const override;
+    
+    // Returns use raycast flag
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual bool IsRaycasted() const override;
+    
+    // Returns use friendly fire flag
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual bool IsFriendlyFireAvailable() const override;
+    
+    // Returns true if weapon can see target, called by Logic
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual bool IsSeeTarget() const override PURE_VIRTUAL_RETURN_METHOD
+    
+    // Returns reload time
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual double GetReloadTime() const override;
+    
+    // Returns started reload time
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual double GetBaseReloadTime() const override;
+    
+    // Sets target to weapon, called by Logic
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual void SetTarget(AActor* target) override;
+    
+    // Returns current attack range
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual double GetAttackRange() const override;
+    
+    // Returns current minimal attack range
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual double GetMinAttackRange() const override;
+    
+    // Returns rotated flag
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual bool IsRotated() const override;
+    
+    // Returns weapon type
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual EWeaponType GetWeaponType() const override;
+    
+    // Returns base weapon
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual double GetBaseDamage() const override;
+    
+    // Returns base attack range
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual double GetBaseAttackRange() const override;
+    
+    // Returns critical chance
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual double GetCriticalChance() const override;
+    
+    // Returns base critical chance
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual double GetBaseCriticalChance() const override;
+    
+    // Returns critical rate
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual double GetCriticalRate() const override;
+    
+    // Returns started critical rate
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual double GetBaseCriticalRate() const override;
+
+protected:
+    
+    // Rotates weapon on target
+    void RotateToTarget();
+
+        /// <summary>
+        /// Rotates weapon on root
+        /// </summary>
+        protected void RotateToRoot()
+        {
+            if (!target && rotatedWeapon)
+            {
+                var lookVector = Vector3.RotateTowards(weaponTransform.forward, weaponTransform.root.forward, rotateSpeed * Time.deltaTime, 0.1f);
+                weaponTransform.rotation = Quaternion.LookRotation(lookVector);
+            }
+        }
+
+        /// <summary>
+        /// Throws raycast to target by origin along direction and checks hit transform
+        /// </summary>
+        /// <returns></returns>
+        protected bool CheckRaycastToTarget(Vector3 origin, Vector3 direction)
+        {
+            var ray = new Ray(origin, direction);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, raycastMask))
+            {
+                return hit.transform == target;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Reload coroutine for reset fire
+        /// </summary>
+        /// <returns></returns>
+        protected IEnumerator FiringDelay()
+        {
+            yield return new WaitForSeconds(currentReloadTime);
+
+            weaponAnimation?.DefaultState();
+            firing = false;
+        }
+
+        /// <summary>
+        /// Returns current weapon parameters
+        /// </summary>
+        /// <returns></returns>
+        public Core.WeaponParameters GetParameters()
+        {
+            var param = new Core.WeaponParameters()
+            {
+                target = target,
+                damage = currentDamage,
+                weaponType = weaponType,
+                critRate = currentCritRate,
+                critChance = currentCritChance,
+                explosionRadius = currentExplosionRadius,
+                owner = transform.root.gameObject
+            };
+
+            return param;
+        }
+
+        /// <summary>
+        /// Recalculates weapon parameter
+        /// </summary>
+        protected override void RecalculateParameter(Core.UnitParameters parameter)
+        {
+            var list = GetParameterEnhanceList(parameter);
+
+            switch (parameter)
+            {
+                case Core.UnitParameters.ExplosionRadius:
+                    {
+                        if (GetParameterAvailable(Core.UnitParameters.ExplosionRadius))
+                        {
+                            currentExplosionRadius = explosionRadius;
+
+                            for (int i = 0; i < list.Count; ++i)
+                            {
+                                currentExplosionRadius += InterpretParameterModifier(explosionRadius, list[i]);
+                            }
+                        }
+                    }
+                    break;
+
+                case Core.UnitParameters.Reload:
+                    {
+                        if (GetParameterAvailable(Core.UnitParameters.Reload))
+                        {
+                            currentReloadTime = reloadTime;
+
+                            for (int i = 0; i < list.Count; ++i)
+                            {
+                                currentReloadTime += InterpretParameterModifier(reloadTime, list[i]);
+                            }
+                        }
+                    }
+                    break;
+
+                case Core.UnitParameters.Damage:
+                    {
+                        if (GetParameterAvailable(Core.UnitParameters.Damage))
+                        {
+                            currentDamage = damage;
+
+                            for (int i = 0; i < list.Count; ++i)
+                            {
+                                currentDamage += InterpretParameterModifier(damage, list[i]);
+                            }
+                        }
+                    }
+                    break;
+
+                case Core.UnitParameters.CritChance:
+                    {
+                        if (GetParameterAvailable(Core.UnitParameters.CritChance))
+                        {
+                            currentCritChance = critChance;
+
+                            for (int i = 0; i < list.Count; ++i)
+                            {
+                                currentCritChance += InterpretParameterModifier(critChance, list[i]);
+                            }
+                        }
+                    }
+                    break;
+
+                case Core.UnitParameters.CritRate:
+                    {
+                        if (GetParameterAvailable(Core.UnitParameters.CritRate))
+                        {
+                            currentCritRate = critRate;
+
+                            for (int i = 0; i < list.Count; ++i)
+                            {
+                                currentCritRate += InterpretParameterModifier(critRate, list[i]);
+                            }
+                        }
+                    }
+                    break;
+
+                case Core.UnitParameters.Range:
+                    {
+                        var logic = transform.root.GetComponent<ILogic>();
+
+                        if (logic != null)
+                        {
+                            if (GetParameterAvailable(Core.UnitParameters.Range))
+                            {
+                                currentAttackRange = attackRange;
+
+                                for (int i = 0; i < list.Count; ++i)
+                                {
+                                    currentAttackRange += InterpretParameterModifier(attackRange, list[i]);
+                                }
+                            }
+
+                            logic.UpdateParameters();
+                        }
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Sets parameter to zero value
+        /// </summary>
+        /// <param name="parameter"></param>
+        protected override void ZeroizeParameter(Core.UnitParameters parameter)
+        {
+            switch (parameter)
+            {
+                case Core.UnitParameters.ExplosionRadius:
+                    currentExplosionRadius = 0;
+                    break;
+
+                case Core.UnitParameters.Reload:
+                    currentReloadTime = Mathf.Infinity;
+                    break;
+
+                case Core.UnitParameters.Damage:
+                    currentDamage = 0;
+                    break;
+
+                case Core.UnitParameters.CritChance:
+                    currentCritChance = 0;
+                    break;
+
+                case Core.UnitParameters.CritRate:
+                    currentCritRate = 1;
+                    break;
+
+                case Core.UnitParameters.Range:
+                    {
+                        var logic = transform.root.GetComponent<ILogic>();
+
+                        if (logic != null)
+                        {
+                            currentAttackRange = 0;
+                            logic.UpdateParameters();
+                        }
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+};
