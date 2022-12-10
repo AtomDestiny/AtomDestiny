bfe3eb562bc12cb7dec943c8e1bb65847e8e0907
Arew <noobterr@yandex.ru>
2022-12-10 08:38:57 +0300

    Base unit behaviour - add IEvent interface

---
+++ Source/AtomDestiny/Event/Event.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include <Runtime/CoreUObject/Public/UObject/Interface.h>
+
+#include "Event.generated.h"
+
+// Unit parameters event
+UENUM(BlueprintType)
+enum EEventType
+{
+    OnHealth,
+    OnShield
+};
+
+UINTERFACE(MinimalAPI, Blueprintable)
+class UEvent : public UInterface
+{
+    GENERATED_BODY()
+};
+
+///
+/// Base interface for callbacks
+///
+class IEvent
+{
+    GENERATED_BODY()
+
+public:
+    
+    // Invokes event with double value and event
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual double Invoke(double value, EEventType eventType) = 0;
+};
