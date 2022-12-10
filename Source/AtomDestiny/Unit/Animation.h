920ee43983b3210ba96070a6727a3f89c9d808ad
Arew <noobterr@yandex.ru>
2022-12-10 20:15:06 +0300

    Base unit behaviour - add unit animation

---
+++ Source/AtomDestiny/Unit/Animation.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include <Runtime/CoreUObject/Public/UObject/Interface.h>
+
+#include "Animation.generated.h"
+
+UINTERFACE(MinimalAPI, Blueprintable)
+class UAnimation : public UInterface
+{
+    GENERATED_BODY()
+};
+
+///
+/// Base interface for unit animation
+///
+class IAnimation
+{
+    GENERATED_BODY()
+    
+public:
+    
+    // Stop animation
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual void Idle() = 0;
+    
+    // Move animation
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual void Walk() = 0;
+    
+    // Attack animation
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual void Attack() = 0;
+};
