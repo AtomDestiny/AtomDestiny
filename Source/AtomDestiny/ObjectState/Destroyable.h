84cca492627f59b48bca81726bb694f1621a8b24
Arew <noobterr@yandex.ru>
2022-12-10 08:48:02 +0300

    Base unit behaviour - add Destroyable interface

---
+++ Source/AtomDestiny/ObjectState/Destroyable.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <Runtime/CoreUObject/Public/UObject/Interface.h>
+
+#include "Destroyable.generated.h"
+
+UINTERFACE(MinimalAPI, Blueprintable)
+class UDestroyable : public UInterface
+{
+    GENERATED_BODY()
+};
+
+///
+/// Base interface for ADObject destroy
+///
+class IDestroyable
+{
+    GENERATED_BODY()
+
+public:
+    
+    // Destroys object
+    UFUNCTION(Meta = (AllowOverride = true))
+    virtual void Destroy() = 0;
+};
