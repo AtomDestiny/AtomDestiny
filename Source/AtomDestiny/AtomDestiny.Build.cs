68bf14e04338b678f17886b899b9a304cfbef6c4
DmitriyEmerald <amid.mik@gmail.com>
2022-12-14 22:18:03 +0300

    Updated non-code files

--- Source/AtomDestiny/AtomDestiny.Build.cs
+++ Source/AtomDestiny/AtomDestiny.Build.cs
@@ -14,11 +14,16 @@ public class AtomDestiny : ModuleRules
             "EnhancedInput",
             "UMG"
         });
-        
-        PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+        PublicIncludePaths.AddRange(new string[] {
+            //"EnhancedInput/Public", "UMG/Public"
+            "AtomDestiny"
+        });
+
+        PrivateDependencyModuleNames.AddRange(new string[] { });
 
         // Uncomment if you are using Slate UI
-        PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+        //PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
         
         // Uncomment if you are using online features
         // PrivateDependencyModuleNames.Add("OnlineSubsystem");
