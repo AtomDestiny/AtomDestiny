4d3a7179fe8f837b1c05f70f75b0562837a4771d
Arew <noobterr@yandex.ru>
2022-12-17 11:21:57 +0300

    Base unit behaviour - add game play folder and struct to store destinations

---
+++ Source/AtomDestiny/Gameplay/GameDestination.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "GameDestination.generated.h"
+
+USTRUCT()
+struct FGameDestination
+{
+    GENERATED_BODY(BlueprintType)
+
+    TWeakObjectPtr<AActor> playerDestination;
+    TWeakObjectPtr<AActor> enemyDestination;
+    TWeakObjectPtr<AActor> playerRespawn;
+    TWeakObjectPtr<AActor> enemyRespawn;
+    TWeakObjectPtr<AActor> neutralDestination;
+};
