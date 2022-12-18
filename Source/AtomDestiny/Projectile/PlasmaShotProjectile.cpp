d1e98b4c8146367d6e135dd7cb072fa7adf5c727
Arew <noobterr@yandex.ru>
2022-12-18 09:22:23 +0300

    Base unit behaviour - add plasma shot projectile. It can be used at Lancer weapon by default.

---
+++ Source/AtomDestiny/Projectile/PlasmaShotProjectile.cpp
@@ -0,0 +1,24 @@
+﻿#include "PlasmaShotProjectile.h"
+
+#include <AtomDestiny/AtomDestinyGameStateBase.h>
+
+#include <AtomDestiny/Core/ObjectPool/ActorPool.h>
+#include <AtomDestiny/Core/ActorComponentUtils.h>
+
+void UPlasmaShotProjectile::Launch()
+{
+    GenerateImpact();
+}
+
+FAsyncCoroutine UPlasmaShotProjectile::GenerateImpact()
+{
+    co_await UE5Coro::Latent::Seconds(m_damageDelay);
+
+    if (m_parameters.target != nullptr)
+    {
+        AtomDestiny::GetGameState(GetOwner())->AddDamage(GET_INTERFACE(Projectile), EProjectileDamageOptions::ImpactPoint);
+        AtomDestiny::ObjectPool::Instance().Spawn(m_impactBlueprint, m_points.impactPosition, FQuat::Identity);
+    }
+
+    AtomDestiny::ObjectPool::Instance().Despawn(MakeWeakObjectPtr(GetOwner()));
+}
