677db1200b10d05d431efa68d0b3ba514959154a
Arew <noobterr@yandex.ru>
2022-12-25 16:33:26 +0300

    AD-5. Core.Tank-unit - add DefaultUnit to create Atom destiny units with basic components

---
+++ Source/AtomDestiny/Templates/DefaultUnit.cpp
@@ -0,0 +1,23 @@
+﻿#include "DefaultUnit.h"
+
+#include <Runtime/Engine/Classes/Components/BoxComponent.h>
+
+#include <AtomDestiny/Unit/UnitState.h>
+#include <AtomDestiny/Unit/UnitParameters.h>
+#include <AtomDestiny/Logic/UnitLogic.h>
+
+ADefaultUnit::ADefaultUnit(const FObjectInitializer& objectInitializer):
+    APawn(objectInitializer)
+{
+    m_boxComponent = objectInitializer.CreateDefaultSubobject<UBoxComponent>(this, TEXT("BoxCollider"));
+    
+    if (RootComponent == nullptr)
+    {
+        RootComponent = m_boxComponent;
+    }
+
+    m_groundPoint = objectInitializer.CreateDefaultSubobject<USceneComponent>(this, TEXT("GroundPoint"));
+    m_unitState = objectInitializer.CreateDefaultSubobject<UUnitState>(this, TEXT("UnitState"));
+    m_unitParameters = objectInitializer.CreateDefaultSubobject<UUnitParameters>(this, TEXT("UnitParameters"));
+    m_unitLogic = objectInitializer.CreateDefaultSubobject<UUnitLogic>(this, TEXT("UnitLogic"));
+}
---
+++ Source/AtomDestiny/Templates/DefaultUnit.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+class UUnitParameters;
+class UUnitState;
+class UUnitLogic;
+class USceneComponent;
+
+#include <Runtime/Engine/Classes/GameFramework/Pawn.h>
+
+#include "DefaultUnit.generated.h"
+
+///
+/// Represents default unit with basic components.
+/// It consists of Box collider, Unit parameters (heath and defence),
+/// Unit state (massive API to unit info and control), Ground point, Movement component and
+/// Unit logic (AI control component).
+///
+UCLASS(BlueprintType, Blueprintable, meta=(ShortTooltip="Default unit with basic needed components to participate at Atom Destiny game"))
+class ADefaultUnit : public APawn
+{
+    GENERATED_BODY()
+    
+public:
+    explicit ADefaultUnit(const FObjectInitializer& objectInitializer = FObjectInitializer::Get());
+
+protected:
+    TObjectPtr<USceneComponent> m_boxComponent;
+    TObjectPtr<USceneComponent> m_groundPoint;
+    TObjectPtr<UUnitState> m_unitState;
+    TObjectPtr<UUnitLogic> m_unitLogic;
+    TObjectPtr<UUnitParameters> m_unitParameters;
+};
