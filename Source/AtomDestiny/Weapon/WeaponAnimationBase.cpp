2e364ef0fdc61afe109cac9eb3290c8a939739e7
Arew <noobterr@yandex.ru>
2023-01-01 18:38:53 +0300

    AD-9. Core. Tank cannon animation - implement weapon base animation class

---
+++ Source/AtomDestiny/Weapon/WeaponAnimationBase.cpp
@@ -0,0 +1,19 @@
+﻿#include "WeaponAnimationBase.h"
+
+#include <AtomDestiny/Core/Logger.h>
+
+UWeaponAnimationBase::UWeaponAnimationBase(const FObjectInitializer& objectInitializer):
+    UActorComponent(objectInitializer)
+{
+    bWantsInitializeComponent = true;
+}
+
+void UWeaponAnimationBase::InitializeComponent()
+{
+    Super::InitializeComponent();
+
+    if (!m_skeletalMeshComponent.IsValid())
+    {
+        LOG_ERROR(TEXT("Skeletal animation mesh is not valid at Weapon animation base"));
+    }
+}
