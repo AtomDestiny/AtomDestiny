0c5cd8d6898cdd41a0678ce8e01c14a2c2d26f79
Emerald777 <amid_mik@mail.ru>
2023-01-03 21:01:23 +0300

    Healthbar widget fixes

--- Source/AtomDestiny/UI/HealthBar.cpp
+++ Source/AtomDestiny/UI/HealthBar.cpp
@@ -13,3 +13,13 @@ void UHealthBar::SetEnergyPercent(float value)
 {
     m_energy->SetPercent(value);
 }
+
+void UHealthBar::SetHealthVisible(bool visible)
+{
+    m_health->SetVisibility(visible ? ESlateVisibility::Visible : ESlateVisibility::Hidden);
+}
+
+void UHealthBar::SetEnergyVisible(bool visible)
+{
+    m_energy->SetVisibility(visible ? ESlateVisibility::Visible : ESlateVisibility::Hidden);
+}
