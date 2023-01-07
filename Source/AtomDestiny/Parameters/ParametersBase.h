0af93fac0a6114dcfc54009536ea868b328c8ab7
Emerald777 <amid_mik@mail.ru>
2023-01-07 12:25:19 +0300

    Fixes after merge

--- Source/AtomDestiny/Parameters/ParametersBase.h
+++ Source/AtomDestiny/Parameters/ParametersBase.h
@@ -1,22 +1,22 @@
 ﻿#pragma once
 
 #include <AtomDestiny/Core/ADObject/ADObject.h>
-#include <AtomDestiny/ObjectState/ObjectState.h>
+#include <AtomDestiny/Parameters/Parameters.h>
 
 #include <UI/HealthBar.h>
-#include "ObjectStateBase.generated.h"
+#include "ParametersBase.generated.h"
 
 ///
 /// Represents base class for any object
 /// to have a parameters in Atom Destiny world.
 ///
 UCLASS(Abstract)
-class UObjectStateBase : public UADObject, public IObjectState
+class UParametersBase : public UADObject, public IParameters
 {
     GENERATED_BODY()
     
 public:
-    explicit UObjectStateBase(const FObjectInitializer& objectInitializer = FObjectInitializer::Get());
+    explicit UParametersBase(const FObjectInitializer& objectInitializer = FObjectInitializer::Get());
     
     // Adds damage to this object
     virtual void AddDamage(double damage, EWeaponType type, AActor* owner) ABSTRACT_METHOD;
