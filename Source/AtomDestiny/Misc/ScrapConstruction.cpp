73ac2ee2da2b340aa612a6894e2ff1a59e15617f
Arew <noobterr@yandex.ru>
2023-01-12 21:53:15 +0300

    AD-20. Unit. Scrap destroy - add scrap construction on start

---
+++ Source/AtomDestiny/Misc/ScrapConstruction.cpp
@@ -0,0 +1,28 @@
+﻿#include "ScrapConstruction.h"
+
+#include <AtomDestiny/Core/ActorComponentUtils.h>
+
+void UScrapConstruction::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    for (UStaticMeshComponent* component : AtomDestiny::Utils::GetComponents<UStaticMeshComponent>(GetOwner()))
+    {
+        m_localTransforms.Add(MakeWeakObjectPtr(component), component->GetRelativeTransform());
+    }
+}
+
+void UScrapConstruction::SetActive(bool newActive, bool reset)
+{
+    Super::SetActive(newActive, reset);
+
+    if (!newActive)
+    {
+        return;
+    }
+    
+    for (auto& [component, localTransform] : m_localTransforms)
+    {
+        component->SetRelativeTransform(localTransform);
+    }
+}
---
+++ Source/AtomDestiny/Misc/ScrapConstruction.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <Runtime/Core/Public/Containers/Map.h>
+#include <Engine/Classes/Components/ActorComponent.h>
+
+#include "ScrapConstruction.generated.h"
+
+UCLASS(Blueprintable)
+class UScrapConstruction : public UActorComponent
+{
+    GENERATED_BODY()
+
+protected:
+    virtual void BeginPlay() override;
+    virtual void SetActive(bool newActive, bool reset) override;
+    
+private:
+    TMap<TWeakObjectPtr<UStaticMeshComponent>, FTransform> m_localTransforms;
+};
