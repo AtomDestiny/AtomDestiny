8ab463692a7e9d740db2f308a6056bbe50a36372
Emerald777 <amid_mik@mail.ru>
2023-01-19 19:38:12 +0300

    Implemented long laser projectile

---
+++ Source/AtomDestiny/Projectile/LaserProjectile.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Projectile/LaserProjectile.h"
+
+#include <AtomDestiny/AtomDestinyGameStateBase.h>
+
+#include <Niagara/Public/NiagaraComponent.h>
+#include <AtomDestiny/Particle/Particle.h>
+#include <AtomDestiny/Core/ObjectPool/ActorPool.h>
+#include <AtomDestiny/Core/ActorComponentUtils.h>
+
+ULaserProjectile::ULaserProjectile(const FObjectInitializer& objectInitializer):
+	UProjectileBase(objectInitializer)
+{
+	m_niagaraComponent = objectInitializer.CreateDefaultSubobject<UNiagaraComponent>(this, TEXT("NiagaraComponent"));
+}
+
+void ULaserProjectile::Launch()
+{
+	m_niagaraComponent->SetVariableVec3("BeamStartW", GetPoints().startPosition);
+	m_niagaraComponent->SetVariableVec3("BeamEndW", GetPoints().impactPosition);
+	
+	GenerateImpact();
+}
+
+FAsyncCoroutine ULaserProjectile::GenerateImpact()
+{
+	co_await Coroutines::Latent::Seconds(m_damageDelay);
+
+	if (m_parameters.target != nullptr)
+	{
+		AtomDestiny::GetGameState(GetOwner())->AddDamage(GET_INTERFACE(Projectile), EProjectileDamageOptions::ImpactPoint);
+		AtomDestiny::ObjectPool::Instance().Spawn(m_impactBlueprint, m_points.impactPosition, FQuat::Identity);
+	}
+
+	AtomDestiny::ObjectPool::Instance().Despawn(MakeWeakObjectPtr(GetOwner()));
+}
---
+++ Source/AtomDestiny/Projectile/LaserProjectile.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Projectile/ProjectileBase.h"
+#include <UE5Coro/Public/UE5Coro.h>
+#include "LaserProjectile.generated.h"
+
+class UNiagaraComponent;
+
+///
+/// Represents laser shot, that deals damage and spawns impact particle,
+/// driven by raycasted weapon controller.
+//
+UCLASS(ClassGroup=(AtomDestiny), meta=(BlueprintSpawnableComponent))
+class ATOMDESTINY_API ULaserProjectile : public UProjectileBase
+{
+	GENERATED_BODY()
+
+public:
+	explicit ULaserProjectile(const FObjectInitializer& objectInitializer = FObjectInitializer::Get());
+	
+	// Base values initialization to projectile and generating impact blueprint
+	virtual void Launch() override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Niagara component"))
+	TObjectPtr<UNiagaraComponent> m_niagaraComponent;
+	
+private:
+	// Generates impact damage and particle
+	FAsyncCoroutine GenerateImpact();
+    
+	// Time after spawn to add damage
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Add damage delay", AllowPrivateAccess = "true"))
+	double m_damageDelay = 0.1;
+};
