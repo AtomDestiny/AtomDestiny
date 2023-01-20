b37efdce6b6dd33637105783f7df858298fb6e9e
Arew <noobterr@yandex.ru>
2023-01-20 22:33:14 +0300

    AD-26. Projectile. Laser - change lifetime - refactor ULaserProjectile code - fix bug - add child projectile

--- Source/AtomDestiny/Projectile/LaserProjectile.cpp
+++ Source/AtomDestiny/Projectile/LaserProjectile.cpp
@@ -1,10 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "Projectile/LaserProjectile.h"
 
 #include <AtomDestiny/AtomDestinyGameStateBase.h>
 
 #include <Niagara/Public/NiagaraComponent.h>
+
 #include <AtomDestiny/Particle/Particle.h>
 #include <AtomDestiny/Core/ObjectPool/ActorPool.h>
 #include <AtomDestiny/Core/ActorComponentUtils.h>
@@ -20,7 +19,11 @@ void ULaserProjectile::Launch()
     m_niagaraComponent->SetVariableVec3("BeamStartW", GetPoints().startPosition);
     m_niagaraComponent->SetVariableVec3("BeamEndW", GetPoints().impactPosition);
     m_niagaraComponent->SetVariableFloat("BeamLifeTime", m_lifeTime);
+
+    check(m_damageDelay < m_lifeTime);
     GenerateImpact();
+
+    AtomDestiny::ObjectPool::Instance().Despawn(MakeWeakObjectPtr(GetOwner()), m_lifeTime);
 }
 
 FAsyncCoroutine ULaserProjectile::GenerateImpact()
@@ -32,6 +35,4 @@ FAsyncCoroutine ULaserProjectile::GenerateImpact()
         AtomDestiny::GetGameState(GetOwner())->AddDamage(GET_INTERFACE(Projectile), EProjectileDamageOptions::ImpactPoint);
         AtomDestiny::ObjectPool::Instance().Spawn(m_impactBlueprint, m_points.impactPosition, FQuat::Identity);
     }
-
-    AtomDestiny::ObjectPool::Instance().Despawn(MakeWeakObjectPtr(GetOwner()), m_lifeTime);
 }
