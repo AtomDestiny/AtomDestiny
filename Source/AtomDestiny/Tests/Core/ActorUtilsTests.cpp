089db1b551ba05a7aea9465509fd681b07e79899
Nikita Chernyaev <noobterr@yandex.ru>
2023-01-30 17:11:09 +0300

    AD-21. Core. Understand how to write unit tests - add test template to tries

---
+++ Source/AtomDestiny/Tests/Core/ActorUtilsTests.cpp
@@ -0,0 +1,8 @@
+#include "Misc/AutomationTest.h"
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FActorUtilsTests, "AtomDestiny.Core.ActorUtils", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+bool FActorUtilsTests::RunTest(const FString& parameters)
+{
+    return true;
+}
