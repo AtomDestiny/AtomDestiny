96dba336075a3b05140ac1ef71b8b4b0705218e3
Nikita Chernyaev <noobterr@yandex.ru>
2023-02-08 10:39:38 +0300

    AD-21. Core. Understand how to write unit tests - tests moved to private section as recommended UE

--- Source/AtomDestiny/Private/Tests/Core/ActorUtilsTests.cpp
+++ Source/AtomDestiny/Private/Tests/Core/ActorUtilsTests.cpp
@@ -1,5 +1,7 @@
 #include "Misc/AutomationTest.h"
 
+#include <AtomDestiny/Private/Tests/World/TestWorld.h>
+
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(FActorUtilsTests, "AtomDestiny.Core.ActorUtils", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
 
 bool FActorUtilsTests::RunTest(const FString& parameters)
