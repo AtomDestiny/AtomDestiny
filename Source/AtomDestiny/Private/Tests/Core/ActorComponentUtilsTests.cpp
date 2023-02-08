10ec91bd2aeeda031c0548025daa77867b4029c1
Nikita Chernyaev <noobterr@yandex.ru>
2023-02-08 15:25:58 +0300

    AD-21. Core. Understand how to write unit tests - FindComponentByNameTest test added

--- Source/AtomDestiny/Private/Tests/Core/ActorComponentUtilsTests.cpp
+++ Source/AtomDestiny/Private/Tests/Core/ActorComponentUtilsTests.cpp
@@ -7,18 +7,36 @@ IMPLEMENT_SIMPLE_AUTOMATION_TEST(FActorComponentUtilsTests, "AtomDestiny.Core.Ac
     EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
 
 // tests only correct addition component to actor
-static void AddNewComponentToActorTest(FAutomationTestBase& test)
+static void AddNewComponentToActorTest(AtomDestiny::FTestWorld& world, FAutomationTestBase& test)
 {
-    AtomDestiny::FTestWorld world;
-    
-    const TWeakObjectPtr<AActor> actor = world.CreateNewActor(TEXT("TestActor"));
+    const TWeakObjectPtr<AActor> actor = world.CreateNewActor(TEXT("TestActor1"));
     const auto component = AtomDestiny::Utils::AddNewComponentToActor<USceneComponent>(actor);
 
     test.TestTrue(TEXT("Added component should not be nullptr"), component != nullptr);
 }
 
+static void FindComponentByNameTest(AtomDestiny::FTestWorld& world, FAutomationTestBase& test)
+{
+    const TWeakObjectPtr<AActor> actor = world.CreateNewActor(TEXT("TestActor2"));
+    const auto component = AtomDestiny::Utils::AddNewComponentToActor<USceneComponent>(actor);
+
+    FString name;
+    component->AppendName(name);
+
+    test.TestFalse(TEXT("Not empty name"), name.IsEmpty());
+
+    const std::string stdName = std::string(TCHAR_TO_UTF8(*name));
+    const auto expectedComponent = AtomDestiny::Utils::FindComponentByName(actor.Get(), stdName.c_str());
+
+    test.TestTrue(TEXT("Searched component is not nullptr"), expectedComponent != nullptr);
+}
+
 bool FActorComponentUtilsTests::RunTest(const FString& parameters)
 {
-    AddNewComponentToActorTest(*this);
+    AtomDestiny::FTestWorld world;
+    
+    AddNewComponentToActorTest(world, *this);
+    FindComponentByNameTest(world, *this);
+    
     return true;
 }
