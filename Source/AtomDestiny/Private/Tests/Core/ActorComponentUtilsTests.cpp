2387623d93ca66fc358764a7b11dcaa7988dc9ee
Nikita Chernyaev <noobterr@yandex.ru>
2023-02-08 15:02:59 +0300

    AD-21. Core. Understand how to write unit tests - add first test

--- Source/AtomDestiny/Private/Tests/Core/ActorComponentUtilsTests.cpp
+++ Source/AtomDestiny/Private/Tests/Core/ActorComponentUtilsTests.cpp
@@ -1,13 +1,24 @@
 #include "Misc/AutomationTest.h"
 
+#include <AtomDestiny/Core/ActorComponentUtils.h>
 #include <AtomDestiny/Private/Tests/World/TestWorld.h>
 
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(FActorComponentUtilsTests, "AtomDestiny.Core.ActorComponentUtilsTests",
     EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
 
-bool FActorComponentUtilsTests::RunTest(const FString& parameters)
+// tests only correct addition component to actor
+static void AddNewComponentToActorTest(FAutomationTestBase& test)
 {
     AtomDestiny::FTestWorld world;
     
+    const TWeakObjectPtr<AActor> actor = world.CreateNewActor(TEXT("TestActor"));
+    const auto component = AtomDestiny::Utils::AddNewComponentToActor<USceneComponent>(actor);
+
+    test.TestTrue(TEXT("Added component should not be nullptr"), component != nullptr);
+}
+
+bool FActorComponentUtilsTests::RunTest(const FString& parameters)
+{
+    AddNewComponentToActorTest(*this);
     return true;
 }
