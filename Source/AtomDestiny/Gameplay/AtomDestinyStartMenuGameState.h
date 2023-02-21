b35770fb58d6b1144997c78539fff5abb6516f3c
Nikita Chernyaev <noobterr@yandex.ru>
2023-02-21 17:21:51 +0300

    AD-38. Core. Unit setuper and spawner. - AtomDestinyStartMenuGameState skeleton added

---
+++ Source/AtomDestiny/Gameplay/AtomDestinyStartMenuGameState.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <AtomDestiny/AtomDestinyGameStateBase.h>
+
+#include "AtomDestinyStartMenuGameState.generated.h"
+
+UCLASS(Blueprintable)
+class AAtomDestinyStartMenuGameState : public AAtomDestinyGameStateBase
+{
+    GENERATED_BODY()
+};
