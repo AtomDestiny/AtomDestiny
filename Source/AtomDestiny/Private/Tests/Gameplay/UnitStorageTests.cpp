5ce3b5d5f38a36a357074aad97c355517cf9a6ff
Nikita Chernyaev <noobterr@yandex.ru>
2023-03-06 15:05:05 +0300

    AD-60. Gameplay. UnitStorage unit tests - add skeleton

---
+++ Source/AtomDestiny/Private/Tests/Gameplay/UnitStorageTests.cpp
@@ -0,0 +1,6 @@
+#include "Misc/AutomationTest.h"
+
+#include <AtomDestiny/Gameplay/UnitStorage.h>
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FUnitStorageTests, "AtomDestiny.Gameplay.UnitStorage",
+    EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
