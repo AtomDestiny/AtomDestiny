d4974cdc311eec3beec4549df399fd1056939eca
Nikita Chernyaev <noobterr@yandex.ru>
2023-03-07 15:08:16 +0300

    AD-60. Gameplay. UnitStorage unit tests - add IncrementEnum helper to Utils

--- Source/AtomDestiny/Core/Utils.h
+++ Source/AtomDestiny/Core/Utils.h
@@ -4,6 +4,25 @@
 
 #include <AtomDestiny/Core/ObjectPool/ActorPool.h>
 
+namespace AtomDestiny::Utils
+{
+    
+    ///
+    /// Increments Enum value, even it is 'enum class'.
+    /// It would check at compile-time clamp value.
+    /// By default 'None' value of enum should be a final value.
+    ///
+    template <typename Enum>
+    Enum IncrementEnum(const Enum e)
+    {
+        using IntegerType = std::underlying_type_t<Enum>;
+        
+        const auto c = static_cast<Enum>(static_cast<IntegerType>(e) + 1);
+        return c;
+    }
+    
+} // namespace AtomDestiny
+
 #define NOT_NULLPTR_CALL(value, methodName, ...) \
     static_assert(std::is_pointer_v<decltype(value)>, "Type should be a pointer"); \
     if (value != nullptr) { \
