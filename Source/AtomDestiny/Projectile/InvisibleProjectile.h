6d786179e51523dc9e91e0da389435c356788e93
Nikita Chernyaev <noobterr@yandex.ru>
2023-07-20 19:01:03 +0300

    AD-65. Projectile. Plasma shot common projectile - invisible projectile skeleton

---
+++ Source/AtomDestiny/Projectile/InvisibleProjectile.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <AtomDestiny/Projectile/ProjectileBase.h>
+#include <AtomDestiny/Coroutines/Coroutines.h>
+
+#include "InvisibleProjectile.generated.h"
+
+///
+/// Represents 'non-render' shot. It can be used to deal damage and spawns impact particle prefab.
+///
+UCLASS(ClassGroup=(AtomDestiny), meta=(BlueprintSpawnableComponent))
+class AInvisibleProjectile : public AProjectileBase
+{ 
+    GENERATED_BODY()
+};
