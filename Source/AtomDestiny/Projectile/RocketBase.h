09e28339dc91d1a86a8011f47f1594fb1a2dd90d
Nikita Chernyaev <noobterr@yandex.ru>
2023-07-24 22:00:59 +0300

    AD-19. Projectile. Rocket - implement abstract rocket base skeleton

---
+++ Source/AtomDestiny/Projectile/RocketBase.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <AtomDestiny/Projectile/ProjectileBase.h>
+
+#include "RocketBase.generated.h"
+
+///
+/// Represents base abstract rocket projectile.
+/// It contains basic parameters and method that needed to weapon controller.
+/// to control a standard rocket projectile.
+///
+UCLASS(Abstract)
+class ATOMDESTINY_API ARocketBase : public AProjectileBase
+{
+    GENERATED_BODY()
+    
+public:
+    
+};
